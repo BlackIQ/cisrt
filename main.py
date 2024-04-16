294a282ac66117cff95386309f009aba72c4af96
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-04-17 03:23:30 +0330

    Update the main file. Annd a simple try/catch. SSH updated and et

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 import paramiko
 import config as cf
-import method as md
+import methods as md
+import sys
 
-client_ip = input("Insert your client ip: ")
-try:
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    switch_ip = cf.switch_core
-    mac = None
-    
-    while True:
-        ssh.connect(switch_ip, cf.ssh_port, cf.username, cf.password)
-            
-        shell = ssh.invoke_shell()
-        if switch_ip == cf.switch_core:
-            mac = md.showArp(shell, client_ip)[0]
-            
-        port_data = md.showMacAddressTable(shell, mac)
-        port_details = md.showCdpNeighborsDetails(shell, port_data['port'][0])
-        if "DYNAMIC" in port_data['text']:
-            print("switch-ip: {:<18s}- port-details: {:<10s}- Trunk".format(switch_ip, port_data['port'][0]))
-            switch_ip = port_details['ip'][0]
-        else:
-            print("switch-ip: {:<18s}- port-details: {:<10s}- Access".format(switch_ip, port_data['port'][0]))
-            print("client-ip: {:<18s}- Mac Address: {}".format(client_ip, mac))
-            break
+def main():
+    client_ip = input("Insert your client IP: ")
+    try:
+        # Establish SSH connection
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(cf.hostname, cf.hostport, cf.username, cf.password)
+
+        switch_ip = cf.hostname
+        mac = None
         
-except Exception as e:
-    print(e)
-    
-input("Please insert any key to exit ..")
+        while True:
+            if switch_ip == cf.hostname:
+                mac = md.showArp(ssh, client_ip)[0]
+                
+            port_data = md.showMacAddressTable(ssh, mac)
+            port_details = md.showCdpNeighborsDetails(ssh, port_data['port'][0])
+            
+            if "DYNAMIC" in port_data['text']:
+                print("Switch IP: {:<18s} - Port: {:<10s} - Trunk".format(switch_ip, port_data['port'][0]))
+                switch_ip = port_details['ip'][0]
+            else:
+                print("Switch IP: {:<18s} - Port: {:<10s} - Access".format(switch_ip, port_data['port'][0]))
+                print("Client IP: {:<18s} - Mac Address: {}".format(client_ip, mac))
+                break
+            
+    except KeyboardInterrupt:
+        print("\nExiting...")
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        if ssh:
+            ssh.close()
+
+if __name__ == "__main__":
+    main()
